c3b3d995b3fab98590da4a5ac9776e11682edd68
Akshay Jyothi.P <akshayjyothip@gmail.com>
2024-07-04 20:10:40 +0530

    using for loop commit

--- factorial.js
+++ factorial.js
@@ -1,4 +1,4 @@
-
+//using recursion
 function factorial(n) {
     
 
@@ -10,3 +10,21 @@ return n*factorial(n-1)
 }
 
 console.log(factorial(5));
+
+
+
+//using normal loop
+
+const fact=function (n) {
+
+   let num=1;
+
+    for(let i=1;i<=n;i++){
+
+       num*=i
+   
+            }
+            return num;
+}
+
+console.log(fact(5));
